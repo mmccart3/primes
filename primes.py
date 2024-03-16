811fffeb63bc7954da31196bf60d7f27a1c3cc94
Mark McCarthy <mark.joseph.mccarthy@gmail.com>
2024-03-16 09:56:29 +0000

    Update primes.py
    Corrected duplication of 2 and improved performance by step incrfeased to 2 and extra break introduced

--- primes.py
+++ primes.py
@@ -2,14 +2,14 @@ import time
 from math import sqrt
 time.perf_counter()
 
-primes_up_to = 1000000
+primes_up_to = 15485867
 
 # Mark's solution
 start = time.perf_counter()
 
 list_of_primes=[2]
 
-for x in range(2,primes_up_to):
+for x in range(3,primes_up_to,2):
     prime = True
     max = sqrt(x)
     for y in list_of_primes:
@@ -17,6 +17,7 @@ for x in range(2,primes_up_to):
             break
         if (x % y) == 0:
             prime = False
+            break
     if prime:
         list_of_primes.append(x)
 
